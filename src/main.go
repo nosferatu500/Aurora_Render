907af13d00b3bbbe1d800afb0fa1e39792a7febc
Vitaly Shvetsov <vshvetsov@allmax.team>
2018-01-03 00:35:50 +0700

    Improve for createLine algorithm.

--- src/main.go
+++ src/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"AuroraRender/library"
+	// "AuroraRender/library"
 	"image"
 	"image/color"
 	"os"
 	"image/png"
+	"math"
 )
 
 //TODO: Make as const in future release.
@@ -19,7 +20,8 @@ func main() {
 	// Set Color point. Temporary. Only for test.
 	// img.Set(52, 41, red)
 
-	img = library.FlipByVertically(image.RGBA{Pix: img.Pix, Stride: img.Stride, Rect: img.Rect})
+	// Incorrect flip. Need fix.
+	// img = library.FlipByVertically(image.RGBA{Pix: img.Pix, Stride: img.Stride, Rect: img.Rect})
 
 	newImage := createLine(13, 20, 80, 40, image.RGBA{Pix: img.Pix, Stride: img.Stride, Rect: img.Rect}, red)
 	newImage = createLine(20, 13, 40, 80, image.RGBA{Pix: newImage.Pix, Stride: newImage.Stride, Rect: newImage.Rect}, white)
@@ -36,10 +38,41 @@ func main() {
 
 // Return value only for dev test.
 func createLine(x0, y0, x1, y1 int, img image.RGBA, color color.RGBA) image.RGBA {
-	for i := x0; i <= x1; i++ {
-		x := i - x0 / x1 - x0
-		y := y0*(1 - x) + y1*x
-		img.Set(int(x), int(y), color)
+	steep := false
+
+	if math.Abs(float64(x0 - x1)) < math.Abs(float64(y0 - y1)) {
+		x0, y0 = y0, x0
+		x1, y1 = y1, x1
+		steep = true
+	}
+
+	if x0 > x1 {
+		x0, x1 = x1, x0
+		y0, y1 = y1, y0
+	}
+
+	dx := x1 - x0
+	dy := y1 - y0
+
+	derror := math.Abs(float64(dy)) * 2
+	error := 0.0
+
+	y := y0
+
+	for x := x0; x <= x1; x++ {
+		if steep {
+			img.Set(int(y), int(x), color)
+		} else {
+			img.Set(int(x), int(y), color)
+		}
+
+		error += derror
+
+		if error > float64(dx) {
+			if y1 > y0 { y += 1 } else { y += -1 }
+			error -= float64(dx * 2)
+		}
+
 	}
 	return img
 }
