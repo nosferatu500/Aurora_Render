fdba2912f19cf8e5a8ded6e982d5a78dfb35c5d0
Vitaly Shvetsov <vshvetsov@allmax.team>
2018-01-03 22:15:09 +0700

    Re-write calculate triangles func.

--- src/main.go
+++ src/main.go
@@ -40,20 +40,37 @@ func main() {
 	for i := 0; i < count - 20; i++ {
 		face := model.Faces[i]
 
-		fmt.Println(i, face, count)
+		var screenCoords [3]library.Vector2D
+		var worldCoords [3]library.Vector3D
 
 		for j := 0; j < 3; j++ {
-			v0 := model.Verts[face[j] - 1]
-			v1 := model.Verts[face[(j+1)%3] - 1]
-
-			x0 := int((v0.X + 1) * float64(width) / 2)
-			y0 := int((v0.Y + 1) * float64(height) / 2)
-
-			x1 := int((v1.X + 1) * float64(width) / 2)
-			y1 := int((v1.Y + 1) * float64(height) / 2)
-
-			newImage = createLine(x0, y0, x1, y1, image.RGBA{Pix: img.Pix, Stride: img.Stride, Rect: img.Rect}, white)
+			v := model.Verts[face[j] - 1]
+			screenCoords[j] = library.Vector2D{(v.X + 1) * width / 2, (v.Y + 1) * height / 2}
+			worldCoords[j] = v
 		}
+
+		edge1 := worldCoords[2].Subtract(worldCoords[0])
+		edge2 := worldCoords[1].Subtract(worldCoords[0])
+
+		n := edge1.Multiply(edge2)
+
+		n = n.Normalize()
+
+		intensity := n.Multiply(library.Vector3D{0,0,-1})
+
+		// newImage = createLine(screenCoords[1], screenCoords[2], image.RGBA{Pix: img.Pix, Stride: img.Stride, Rect: img.Rect}, white)
+
+		//r := rand.New(rand.NewSource(int64(i)))
+		//randomColor := color.RGBA{uint8(r.Int()), uint8(r.Int()), uint8(r.Int()), 255}
+		//newImage = createTriangle(screenCoords[0], screenCoords[1], screenCoords[2],image.RGBA{Pix: img.Pix, Stride: img.Stride, Rect: img.Rect}, randomColor)
+
+		fmt.Println(intensity)
+
+		if intensity.Z > 0 {
+			intensityColor := color.RGBA{uint8(intensity.Z * 255), uint8(intensity.Z * 255), uint8(intensity.Z * 255), 255}
+			newImage = createTriangle(screenCoords[0], screenCoords[1], screenCoords[2],image.RGBA{Pix: img.Pix, Stride: img.Stride, Rect: img.Rect}, intensityColor)
+		}
+
 	}
 
 	img = library.FlipByVertically(image.RGBA{Pix: newImage.Pix, Stride: newImage.Stride, Rect: newImage.Rect})
@@ -65,132 +82,77 @@ func main() {
 
 }
 
-func createLine(x0, y0, x1, y1 int, img image.RGBA, color color.RGBA) image.RGBA {
+func createLine(p0, p1 library.Vector2D, img image.RGBA, color color.RGBA) image.RGBA {
 	steep := false
 
-	if math.Abs(float64(x0 - x1)) < math.Abs(float64(y0 - y1)) {
-		x0, y0 = y0, x0
-		x1, y1 = y1, x1
+	if math.Abs(float64(p0.X - p1.X)) < math.Abs(float64(p0.Y - p1.Y)) {
+		p0.X, p0.Y = p0.Y, p0.X
+		p1.X, p1.Y = p1.Y, p1.X
 		steep = true
 	}
 
-	if x0 > x1 {
-		x0, x1 = x1, x0
-		y0, y1 = y1, y0
+	if p0.X > p1.X {
+		p0, p1 = p1, p0
 	}
 
-	dx := x1 - x0
-	dy := y1 - y0
-
-	derr := math.Abs(float64(dy)) * 2
-	err := 0
-
-	y := y0
-
-	for x := x0; x <= x1; x++ {
+	for x := p0.X; x <= p1.X; x++ {
+		t := (x-p0.X)/(p1.X-p0.X);
+		y := p0.Y*(1.-t) + p1.Y*t;
 		if steep {
-			img.Set(y, x, color)
+			img.Set(int(y), int(x), color);
 		} else {
-			img.Set(x, y, color)
-		}
-
-		err += int(derr)
-		if err > dx {
-			if y1 > y0 {
-				y++
-			} else {
-				y--
-			}
-			err -= dx * 2
+			img.Set(int(x), int(y), color);
 		}
 	}
 	return img
 }
 
-func createTriangle(t0, t1, t2 library.Vector3D, img image.RGBA, color color.RGBA, zBuffer [width*height]int) image.RGBA {
-	if t0.Y == t1.Y && t0.Y == t2.Y { return img }
+func createTriangle(t0, t1, t2 library.Vector2D, img image.RGBA, color color.RGBA) image.RGBA {
+	if t0.Y > t1.Y { t0, t1 = t1, t0 }
 
-	if t0.Y > t1.Y {
-		t0, t1 = t1, t0
-	}
+	if t0.Y > t2.Y { t0, t2 = t2, t0 }
 
-	if t0.Y > t2.Y {
-		t0, t2 = t2, t0
-	}
-
-	if t1.Y > t2.Y {
-		t1, t2 = t2, t1
-	}
+	if t1.Y > t2.Y { t1, t2 = t2, t1 }
 
 	totalHeight := t2.Y - t0.Y
 
-	for i := 0.0; i < totalHeight; i++ {
-		secondHalf := i > t1.Y - t0.Y || t1.Y == t0.Y
-
-		segmentHeight := 0.0
-
-		if secondHalf {
-			segmentHeight = t2.Y - t1.Y
-		} else {
-			segmentHeight = t1.Y - t0.Y
-		}
-
-		alpha := i / totalHeight
-
-		beta := 0.0
-
-		if secondHalf {
-			beta = (i - (t1.Y - t0.Y)) / segmentHeight
-		} else {
-			beta = i / segmentHeight
-		}
+	for y := t0.Y; y <= t1.Y; y++ {
+		segmentHeight := t1.Y-t0.Y+1
+		alpha := (y-t0.Y) / totalHeight
+		beta := (y-t0.Y) / segmentHeight
 
 		firstStepA := t2.Subtract(t0)
 		secondStepA := firstStepA.MultiplyScalar(alpha)
 		A := t0.Add(secondStepA)
 
-		B := library.Vector3D{0,0, 0}
+		firstStepB := t1.Subtract(t0)
+		secondStepB := firstStepB.MultiplyScalar(beta)
+		B := t0.Add(secondStepB)
 
-		if secondHalf {
-			subVector := t2.Subtract(t1)
-			multiplyVector := subVector.MultiplyScalar(beta)
-			B = t1.Add(multiplyVector)
-		} else {
-			subVector := t1.Subtract(t0)
-			multiplyVector := subVector.MultiplyScalar(beta)
-			B = t0.Add(multiplyVector)
+		if A.X > B.X { A, B = B, A }
+
+		for j := A.X; j <= B.X; j++ {
+			img.Set(int(j), int(y), color)
 		}
+	}
 
-		if A.X > B.X {
-			A, B = B, A
-		}
+	for y := t1.Y; y <= t2.Y; y++ {
+		segmentHeight := t2.Y-t1.Y+1
+		alpha := (y-t0.Y) / totalHeight
+		beta := (y-t1.Y) / segmentHeight
 
-		for j := A.X; j <=B.X; j++ {
-			var phi = 0.0
+		firstStepA := t2.Subtract(t0)
+		secondStepA := firstStepA.MultiplyScalar(alpha)
+		A := t0.Add(secondStepA)
 
-			if B.X == A.X {
-				phi = 1.0
-			} else {
-				phi = (j - A.X) / (B.X - A.X)
-			}
-			subVector := B.Subtract(A)
-			multiplyVector := subVector.MultiplyScalar(phi)
-			P := A.Add(multiplyVector)
+		firstStepB := t2.Subtract(t1)
+		secondStepB := firstStepB.MultiplyScalar(beta)
+		B := t1.Add(secondStepB)
 
-			P.X = j
-			P.Y = t0.Y + i
+		if A.X > B.X { A, B = B, A }
 
-			idx := int(j + P.Y * width)
-
-			if idx <= width*height {
-				if zBuffer[idx] < int(P.Z) {
-					zBuffer[idx] = int(P.Z)
-					img.Set(int(P.X), int(P.Y), color)
-				}
-			}
-
-			img.Set(int(P.X), int(P.Y), color)
-
+		for j := A.X; j <= B.X; j++ {
+			img.Set(int(j), int(y), color)
 		}
 	}
 	return img
